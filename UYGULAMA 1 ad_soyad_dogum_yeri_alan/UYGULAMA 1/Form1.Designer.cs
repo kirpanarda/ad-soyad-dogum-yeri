893408b0c5ab7beab14ec70f2bc64814d662e0f6
kirpanarda <kirpanarda@gmail.com>
2021-11-12 14:08:08 +0300

    .

---
+++ UYGULAMA 1 ad_soyad_dogum_yeri_alan/UYGULAMA 1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace UYGULAMA_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1ad = new System.Windows.Forms.Label();
+            this.label2soyad = new System.Windows.Forms.Label();
+            this.label3dogum_yeri = new System.Windows.Forms.Label();
+            this.label4alan = new System.Windows.Forms.Label();
+            this.label5ad = new System.Windows.Forms.Label();
+            this.label6soyad = new System.Windows.Forms.Label();
+            this.label7dogum_yeri = new System.Windows.Forms.Label();
+            this.label8alan = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1ad
+            // 
+            this.label1ad.AutoSize = true;
+            this.label1ad.Location = new System.Drawing.Point(24, 90);
+            this.label1ad.Name = "label1ad";
+            this.label1ad.Size = new System.Drawing.Size(23, 15);
+            this.label1ad.TabIndex = 0;
+            this.label1ad.Text = "AD";
+            // 
+            // label2soyad
+            // 
+            this.label2soyad.AutoSize = true;
+            this.label2soyad.Location = new System.Drawing.Point(24, 138);
+            this.label2soyad.Name = "label2soyad";
+            this.label2soyad.Size = new System.Drawing.Size(44, 15);
+            this.label2soyad.TabIndex = 1;
+            this.label2soyad.Text = "SOYAD";
+            // 
+            // label3dogum_yeri
+            // 
+            this.label3dogum_yeri.AutoSize = true;
+            this.label3dogum_yeri.Location = new System.Drawing.Point(24, 187);
+            this.label3dogum_yeri.Name = "label3dogum_yeri";
+            this.label3dogum_yeri.Size = new System.Drawing.Size(77, 15);
+            this.label3dogum_yeri.TabIndex = 2;
+            this.label3dogum_yeri.Text = "DOĞUM YERİ";
+            // 
+            // label4alan
+            // 
+            this.label4alan.AutoSize = true;
+            this.label4alan.Location = new System.Drawing.Point(24, 249);
+            this.label4alan.Name = "label4alan";
+            this.label4alan.Size = new System.Drawing.Size(38, 15);
+            this.label4alan.TabIndex = 3;
+            this.label4alan.Text = "ALAN";
+            // 
+            // label5ad
+            // 
+            this.label5ad.AutoSize = true;
+            this.label5ad.Location = new System.Drawing.Point(632, 90);
+            this.label5ad.Name = "label5ad";
+            this.label5ad.Size = new System.Drawing.Size(38, 15);
+            this.label5ad.TabIndex = 4;
+            this.label5ad.Text = "label5";
+            // 
+            // label6soyad
+            // 
+            this.label6soyad.AutoSize = true;
+            this.label6soyad.Location = new System.Drawing.Point(632, 138);
+            this.label6soyad.Name = "label6soyad";
+            this.label6soyad.Size = new System.Drawing.Size(38, 15);
+            this.label6soyad.TabIndex = 5;
+            this.label6soyad.Text = "label6";
+            // 
+            // label7dogum_yeri
+            // 
+            this.label7dogum_yeri.AutoSize = true;
+            this.label7dogum_yeri.Location = new System.Drawing.Point(632, 187);
+            this.label7dogum_yeri.Name = "label7dogum_yeri";
+            this.label7dogum_yeri.Size = new System.Drawing.Size(38, 15);
+            this.label7dogum_yeri.TabIndex = 6;
+            this.label7dogum_yeri.Text = "label7";
+            // 
+            // label8alan
+            // 
+            this.label8alan.AutoSize = true;
+            this.label8alan.Location = new System.Drawing.Point(632, 249);
+            this.label8alan.Name = "label8alan";
+            this.label8alan.Size = new System.Drawing.Size(38, 15);
+            this.label8alan.TabIndex = 7;
+            this.label8alan.Text = "label8";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(244, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(244, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(244, 249);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8alan);
+            this.Controls.Add(this.label7dogum_yeri);
+            this.Controls.Add(this.label6soyad);
+            this.Controls.Add(this.label5ad);
+            this.Controls.Add(this.label4alan);
+            this.Controls.Add(this.label3dogum_yeri);
+            this.Controls.Add(this.label2soyad);
+            this.Controls.Add(this.label1ad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1ad;
+        private System.Windows.Forms.Label label2soyad;
+        private System.Windows.Forms.Label label3dogum_yeri;
+        private System.Windows.Forms.Label label4alan;
+        private System.Windows.Forms.Label label5ad;
+        private System.Windows.Forms.Label label6soyad;
+        private System.Windows.Forms.Label label7dogum_yeri;
+        private System.Windows.Forms.Label label8alan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UYGULAMA 1 ad_soyad_dogum_yeri_alan/UYGULAMA 1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UYGULAMA_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5ad.Text = textBox1.Text;
+            label6soyad.Text = textBox2.Text;
+            label7dogum_yeri.Text = textBox3.Text;
+            label8alan.Text = textBox4.Text;
+        }
+    }
+}
